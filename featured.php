4fcf7c93d4dc2b788447e5df962244294aa49c45
Sekk <kicknclash@gmail.com>
2017-08-11 04:49:25 -0300

    .

--- featured.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-$main_content .= '
-<div style="position: relative; top: -2px; margin-bottom: 2px;" >
-                        <center><b>IP: SEU IP AQUI Versão 8.60 Port: 7171</b></center>
-                    </div>
- <center>EDITE ISSO NO featured.php. </center>
-';
-$main_content .= '</table>';
-?>
